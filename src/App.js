e0f250a02a81973d54661d94c818f8e38bbf1015
antaresofficial <antaresofficial@icloud.com>
2022-01-26 12:28:28 +0500

    eded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div style={{ gridArea: "aa" }}></div>
+      <div style={{ gridArea: "ab" }}></div>
+      <div style={{ gridArea: "ac" }}></div>
+      <div style={{ gridArea: "ad" }}></div>
+      <div style={{ gridArea: "ae" }}></div>
+      <div style={{ gridArea: "af" }}></div>
+      <div style={{ gridArea: "ag" }}></div>
+      <div style={{ gridArea: "ah" }}></div>
+      <div style={{ gridArea: "ai" }}></div>
+      <div style={{ gridArea: "al" }}></div>
+      <div style={{ gridArea: "am" }}></div>
+      <div style={{ gridArea: "an" }}></div>
+      <div style={{ gridArea: "ao" }}></div>
+      <div style={{ gridArea: "ap" }}></div>
+      <div style={{ gridArea: "aq" }}></div>
+      <div style={{ gridArea: "as" }}></div>
+      <div style={{ gridArea: "at" }}></div>
+      <div style={{ gridArea: "au" }}></div>
+      <div style={{ gridArea: "av" }}></div>
+      <div style={{ gridArea: "aw" }}></div>
+      <div style={{ gridArea: "ax" }}></div>
+      <div style={{ gridArea: "ay" }}></div>
+      <div style={{ gridArea: "az" }}></div>
+      <div style={{ gridArea: "ba" }}></div>
+      <div style={{ gridArea: "bb" }}></div>
+      <div style={{ gridArea: "bc" }}></div>
+      <div style={{ gridArea: "bd" }}></div>
+      <div style={{ gridArea: "be" }}></div>
+      <div style={{ gridArea: "bf" }}></div>
+      <div style={{ gridArea: "bg" }}></div>
+      <div style={{ gridArea: "bh" }}></div>
+      <div style={{ gridArea: "bi" }}></div>
+      <div style={{ gridArea: "bj" }}></div>
+      <div style={{ gridArea: "bk" }}></div>
+      <div style={{ gridArea: "bl" }}></div>
+      <div style={{ gridArea: "bm" }}></div>
+      <div style={{ gridArea: "bn" }}></div>
+      <div style={{ gridArea: "bo" }}></div>
+      <div style={{ gridArea: "bp" }}></div>
+      <div style={{ gridArea: "bq" }}></div>
+      <div style={{ gridArea: "br" }}></div>
+      <div style={{ gridArea: "bs" }}></div>
     </div>
   );
 }
